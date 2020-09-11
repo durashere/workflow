86137d07f840eaab43b74c3fd24f72d5ebcc3e71
durashere <walendes@gmail.com>
2020-09-11 13:13:20 +0200

    automatic build

---
+++ client/postbuild.js
@@ -0,0 +1,28 @@
+const { exec } = require("child_process");
+const os = require("os");
+
+if (os.platform() === "win32") {
+  exec("move build ../server/", (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+  });
+} else {
+  exec("mv build ../server/", (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+  });
+}
