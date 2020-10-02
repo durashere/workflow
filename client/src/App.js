85567714125c0c369e7da1ddf262f784fbc496d7
Krzysztof Durek <walendes@gmail.com>
2020-10-02 15:39:43 +0200

    feat: site loading bakcdrop

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,10 @@ import {
   Redirect,
 } from "react-router-dom";
 
+import { makeStyles } from "@material-ui/core/styles";
+import Backdrop from "@material-ui/core/Backdrop";
+import CircularProgress from "@material-ui/core/CircularProgress";
+
 import { AuthProvider, AuthContext } from "./context/AuthContext";
 import { FetchProvider } from "./context/FetchContext";
 import { SnackbarProvider } from "./context/SnackbarContext";
@@ -20,13 +24,23 @@ const Dashboard = lazy(() => import("./Dashboard"));
 const UserList = lazy(() => import("./User/UserList"));
 const UserForm = lazy(() => import("./User/UserForm"));
 const TonerListAdmin = lazy(() => import("./Toner/TonerListAdmin"));
-// const TonerForm = lazy(() => import("./Toner/TonerForm"));
 const CmsHelper = lazy(() => import("./Cms/CmsHelper"));
 
+const useStyles = makeStyles((theme) => ({
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: "#fff",
+  },
+}));
+
 function App() {
+  const classes = useStyles();
+
   const LoadingFallback = () => (
     <AppShell>
-      <div>Loading...</div>
+      <Backdrop className={classes.backdrop} open={true}>
+        <CircularProgress color="inherit" />
+      </Backdrop>
     </AppShell>
   );
 
