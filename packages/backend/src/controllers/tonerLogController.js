9e83b3c57839ce032a9f7cbb59a75c8f45609433
durashere <walendes@gmail.com>
2020-11-02 12:08:04 +0100

    refactor: logs as subdocuments

--- packages/backend/src/controllers/tonerLogController.js
+++ packages/backend/src/controllers/tonerLogController.js
@@ -1,35 +1,41 @@
 /* eslint-disable no-underscore-dangle */
 const tonersLogsRouter = require("express").Router();
 const Toner = require("../models/tonerModel");
-const TonerLog = require("../models/tonerLogModel");
 
 const { requireAuth, requireAdmin } = require("../util");
 
-tonersLogsRouter.get("/", requireAuth, async (request, response) => {
-  try {
-    const tonersLogs = await TonerLog.find({});
+// tonersLogsRouter.get("/", requireAuth, async (request, response) => {
+//   try {
+//     const tonersLogs = await TonerLog.find({});
 
-    const tonersToJson = tonersLogs.map((tonerLog) => tonerLog.toJSON());
+//     const tonersToJson = tonersLogs.map((tonerLog) => tonerLog.toJSON());
 
-    return response.json(tonersToJson);
-  } catch (error) {
-    return response.status(400).json({
-      message: "There was a problem fetching toners logs",
-    });
-  }
-});
+//     return response.json(tonersToJson);
+//   } catch (error) {
+//     return response.status(400).json({
+//       message: "There was a problem fetching toners logs",
+//     });
+//   }
+// });
 
 tonersLogsRouter.post("/", requireAuth, async (request, response) => {
   try {
-    const data = request.body;
+    const { toner, newTonerLog } = request.body;
 
-    const newTonerLog = new TonerLog(data.toner_log);
-    await newTonerLog.save();
+    if (toner.amount <= 0) {
+      return response
+        .status(400)
+        .json({ message: "There are no toners this type" });
+    }
 
     const updatedToner = await Toner.findByIdAndUpdate(
-      { _id: data.toner_id },
-      { amount: data.toner_amount, $push: { logs: newTonerLog } },
-    ).populate("logs");
+      { _id: toner._id },
+      {
+        amount: toner.amount - 1,
+        $push: { logs: newTonerLog },
+      },
+      { new: true },
+    );
 
     return response.status(201).json({
       message: "Toner log created!",
--- packages/backend/src/models/tonerLogModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const tonerLogSchema = new mongoose.Schema({
-  log_user: { type: String },
-  log_time: { type: Date },
-});
-
-const TonerLog = mongoose.model("TonerLog", tonerLogSchema, "tonerslogs");
-module.exports = TonerLog;
